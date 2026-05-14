f661b41472a55a5c9c2caca7a1f357cb15e0e3c8
yoonseo <lys8167@gmail.com>
2026-05-14 18:46:38 +0900

    ✨feat: Initial project setup with Spring Boot, Kotlin, and Gradle

---
+++ src/main/kotlin/com/medilux/blt/BltApplication.kt
@@ -0,0 +1,11 @@
+package com.medilux.blt
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BltApplication
+
+fun main(args: Array<String>) {
+	runApplication<BltApplication>(*args)
+}
---
+++ src/test/kotlin/com/medilux/blt/BltApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.medilux.blt
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BltApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
